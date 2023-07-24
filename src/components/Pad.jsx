e7c9726b0dbcea9b2638baeca417b9b584b0a557
HenriBDev <henribdev@gmail.com>
2023-07-23 23:40:47 -0300

    :truck: build - Moved images to public folder
    - Moved images to public folder to allow production building test using vite preview

--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -6,7 +6,7 @@ function Pad({type, onClick, padText, imgName, imgDescription, visibilityClass, 
     return <div type={type} className="pad" onClick={onClick}>
         <div className="pad__background body2">
             {type == "image" && 
-                <img className={`pad__display ${visibilityClass}`} src={`src/assets/imgs/${imgName}`} alt={imgDescription} />
+                <img className={`pad__display ${visibilityClass}`} src={imgName} alt={imgDescription} />
             }
             {(type == "text-line" || type == "text-paragraph") &&
                 padText + cursor
